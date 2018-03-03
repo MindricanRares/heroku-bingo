7518cc17cdb01f0b24f5fe3cab81edf0d33272e5
Rares <mindricanmihai@gmail.com>
2018-03-03 14:59:32 +0200

    - moved server to different repo

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import io from 'socket.io-client';
-// const  socket = io.connect('http://localhost');
-var host = location.origin;
-const  socket =io.connect(host, {port: PORT, transports: ["websocket"]});
+const  socket = io.connect('https://heroku-bingo-server.herokuapp.com/');
+
 function subscribeToResults(cb) {
   socket.on('results', results => cb(null, results));
   socket.emit('subscribeToResults', 1000);
