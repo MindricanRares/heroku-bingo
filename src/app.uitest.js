98d8f3600bf4e7ddbc8464b6c3627a1867bdefe2
Rares <mindricanmihai@gmail.com>
2018-03-03 18:20:01 +0200

    - working on integration with travis ci

--- src/app.uitest.js
+++ src/app.uitest.js
@@ -7,14 +7,14 @@ fixture `Getting Started`// declare the fixture
 //then create a test and place your code there
 test('Title Test', async t => {
 
-    const article = Selector('.title');
+    const article = Selector('#root > div > header > h1');
     await t.expect(article.innerText).eql("Office bingo");
 });
 
 test('Button can`t be clicked more than 5 times', async t => {
 
-    const addButton = Selector('#root > div > div > div.btn-group.btn-matrix > div:nth-child(2) > div > button.btn.btn-success.bingo-card-btn');
-    const btnClickedNumber = Selector('#root > div > div > div.btn-group.btn-matrix > div:nth-child(2) > div > p:nth-child(2)');
+    const addButton = Selector('#root > div > div > div > div.col-10.gameBoard > div.btn-group.btn-matrix > div:nth-child(1) > div > button.btn.btn-success.bingo-card-btn');
+    const btnClickedNumber = Selector('#root > div > div > div > div.col-10.gameBoard > div.btn-group.btn-matrix > div:nth-child(1) > div > p:nth-child(2)');
     await t.click(addButton);
     await t.click(addButton);
     await t.click(addButton);
